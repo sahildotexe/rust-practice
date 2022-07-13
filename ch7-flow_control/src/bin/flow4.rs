acd8c9faefee617f968212bf910acb8e987ab466
sahildotexe <sahilkaling@gmail.com>
2022-07-13 22:59:02 +0530

    ch7 done

---
+++ ch7-flow_control/src/bin/flow4.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let names = [String::from("liming"), String::from("hanmeimei")];
+    for name in &names {
+        // do something with name...
+    }
+
+    println!("{:?}", names);
+
+    let numbers = [1, 2, 3];
+    // the elements in numbers are Copy，so there is no move here
+    for n in numbers {
+        // do something with name...
+    }
+
+    println!("{:?}", numbers);
+} 
